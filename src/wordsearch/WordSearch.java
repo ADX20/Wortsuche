f176243af154f717992808c6abd2f4e57501b36c
4ND1 <43019796+ADX20@users.noreply.github.com>
2022-01-12 20:04:51 +0100

    added output for the line in which the word was found
    The code now return the line in which the word was found

--- src/wordsearch/WordSearch.java
+++ src/wordsearch/WordSearch.java
@@ -4,19 +4,24 @@ public class WordSearch {
 
 	public static void main(String[] args) {
 
-		char[][] field = { 	{ 'K', 'Ä', 'S', 'E', 'M', 'S', 'M', 'E', 'C', 'B' },
-							{ 'Z', 'U', 'C', 'K', 'E', 'R', 'R', 'E', 'I', 'S' },
-							{ 'Z', 'H', 'E', 'I', 'E', 'R', 'H', 'F', 'G', 'J' },
-							{ 'F', 'I', 'S', 'C', 'H', 'S', 'M', 'R', 'L', 'O' },
-							{ 'H', 'O', 'N', 'I', 'G', 'S', 'R', 'T', 'R', 'G' },
-							{ 'M', 'A', 'R', 'M', 'E', 'L', 'A', 'D', 'E', 'H' },
-							{ 'B', 'U', 'T', 'T', 'E', 'R', 'U', 'A', 'N', 'U' },
-							{ 'N', 'U', 'D', 'E', 'L', 'N', 'E', 'H', 'M', 'R' },
-							{ 'W', 'U', 'R', 'S', 'T', 'B', 'R', 'O', 'T', 'T' },
-							{ 'A', 'F', 'F', 'L', 'E', 'I', 'S', 'C', 'H', 'F' } };
+		char[][] field = { { 'K', 'Ä', 'S', 'E', 'M', 'S', 'M', 'E', 'C', 'B' },
+						   { 'Z', 'U', 'C', 'K', 'E', 'R', 'R', 'E', 'I', 'S' },
+						   { 'Z', 'H', 'E', 'I', 'E', 'R', 'H', 'F', 'G', 'J' },
+						   { 'F', 'I', 'S', 'C', 'H', 'S', 'M', 'R', 'L', 'O' },
+						   { 'H', 'O', 'N', 'I', 'G', 'S', 'R', 'T', 'R', 'G' },
+						   { 'M', 'A', 'R', 'M', 'E', 'L', 'A', 'D', 'E', 'H' },
+						   { 'B', 'U', 'T', 'T', 'E', 'R', 'U', 'A', 'N', 'U' },
+						   { 'N', 'U', 'D', 'E', 'L', 'N', 'E', 'H', 'M', 'R' },
+						   { 'W', 'U', 'R', 'S', 'T', 'B', 'R', 'O', 'T', 'T' },
+						   { 'A', 'F', 'F', 'L', 'E', 'I', 'S', 'C', 'H', 'F' } };
 
-		String Searching_Word = "fisch".toUpperCase();
+		String Searching_Word = "fleisch".toUpperCase();
+		System.out.println(solve(field, Searching_Word));
+	}
 
+	public static int solve(char[][] field, String Searching_Word) {
+
+		String Possible_Solution = "";
 		for (int i = 0; i < field.length; i++) {
 			// System.out.println(field[i].length);
 			for (int j = 0; j < field[i].length; j++) {
@@ -25,20 +30,31 @@ public class WordSearch {
 					for (int k = 0; k < Searching_Word.length(); k++) {
 						try {
 							if (field[i][j + k] == Searching_Word.charAt(k)) {
-								System.out.print(field[i][(j + k)]);
+								// System.out.print(field[i][(j + k)]);
+								Possible_Solution += field[i][j + k];
+								if (Searching_Word.equals(Possible_Solution)) {
+									try {
+										return i + 1;
+									} catch (Exception e) {
+										return i;
+									}
+								}
 							} else {
 								break;
 							}
-						} catch (Exception e) {
+						} catch (Exception f) {
 
 						}
 					}
-					System.out.println();
+					// System.out.println(Possible_Solution);
+					// System.out.println();
+					Possible_Solution = "";
 
 				}
 			}
 
 		}
+		return -1;
 
 	}
 
